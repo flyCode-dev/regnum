f80c632691355869652d04ccc22863d6abd0a48a
Rouben Shekspir <sheksir55@gmail.com>
2017-03-14 12:36:09 +0400

    new synax implemented

--- src/index.js
+++ src/index.js
@@ -1,58 +1,61 @@
 import _ from 'lodash';
 let debug = false;
 
-const startSymbol = '$';
-const endSymbol = '$';
-
-
-String.prototype.regnumMatch = function () {
-  this.match.apply(arguments);
-};
-
-/**
- *
- * @param input
- * @param RegnumExpresson
- * @param map
- * @param options
- * @returns {{}}
- */
-export default function (input, RegnumExpresson, map, options) {
-  let output = {};
-
-  let keys = Object.keys(map);
-  keys = keys.map(key => {
-    let keyword = startSymbol + key + endSymbol;
-    return {
-      key,
-      index: RegnumExpresson.indexOf(keyword)
-    };
-  });
-
-  keys = _.omit(keys, {index: -1});
-  keys = _.sortBy(keys, 'index');
-
-  _.each(keys, o => {
-    let keyword = startSymbol + o.key + endSymbol;
-    RegnumExpresson = RegnumExpresson.replace(keyword, map[o.key]);
-  });
-
-  let matchedArray = input.match(new RegExp(RegnumExpresson, options));
-
-  if (debug) {
-    console.log('Input            ====>', input);
-    console.log('Result           ====>', matchedArray);
-    console.log('Compiled RegExp  ====>', RegnumExpresson);
-    console.log('Options          ====>', options);
+export default class regnum {
+  /**
+   *
+   * @param map
+   * @param expression
+   * @param options
+   */
+  constructor(expression, map, options) {
+    this.startSymbol = '$';
+    this.endSymbol = '$';
+    this.expression = expression;
+    this.map = map;
+    this.map = map;
+    this.options = options;
   }
 
-  if (matchedArray && matchedArray.length) {
-    matchedArray.splice(0, 1);
-    _.each(keys, (o, index) => {
-      output[o.key] = matchedArray[index];
+  getRegExp() {
+    this.keys = Object.keys(this.map);
+    this.keys = this.keys.map(key => {
+      let keyword = this.startSymbol + key + this.endSymbol;
+      return {
+        key,
+        index: this.expression.indexOf(keyword)
+      };
     });
+
+    this.keys = _.omit(this.keys, {index: -1});
+    this.keys = _.sortBy(this.keys, 'index');
+
+    _.each(this.keys, o => {
+      let
+        keyword = this.startSymbol + o.key + this.endSymbol;
+      this.expression = this.expression.replace(keyword, this.map[o.key]);
+    });
+    return new RegExp(this.expression, this.options);
   }
 
+  match(input) {
+    let output = {};
+    let matchedArray = input.match(this.getRegExp());
 
-  return output;
+    if (debug) {
+      console.log('Input            ====>', input);
+      console.log('Result           ====>', matchedArray);
+      console.log('Compiled RegExp  ====>', expression);
+      console.log('Options          ====>', this.options);
+    }
+
+    if (matchedArray && matchedArray.length) {
+      matchedArray.splice(0, 1);
+      _.each(this.keys, (o, index) => {
+        output[o.key] = matchedArray[index];
+      });
+    }
+    return output;
+  }
 }
+
--- test/index.js
+++ test/index.js
@@ -1,33 +1,37 @@
 import test from 'tape';
-import regnum from '../src';
+import Regnum from '../src';
 
 test('regnum', (t) => {
-  'asdasdas'.regnumMatch()
-  // let regnumObject = {};
-  // t.plan(4);
-  //
-  // regnumObject = {number: '[0-9]*'};
-  // t.deepEqual({number: '22'}, regnum('22', '($number$)', regnumObject), 'simple test');
-  //
-  // regnumObject = {number: '[0-9]*'};
-  // t.deepEqual({number: '22'}, regnum('Bending Unit 22', '[a-zA-Z ]*($number$)', regnumObject), 'testing options');
-  //
-  // regnumObject = {number: '[0-9]*', name: '[a-zA-Z]*'};
-  // t.deepEqual({
-  //   name: 'Bender',
-  //   number: '22'
-  // }, regnum('Bender Bending Rodríguez designated in-universe as Bending Unit 22', '($name$).* ($number$)', regnumObject), '2 object return test');
-  //
-  //
-  // regnumObject = {
-  //   username: '[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+',
-  //   domain: '[a-zA-Z0-9-]+',
-  //   zone: '(?:\.[a-zA-Z0-9-]+)*'
-  // };
-  // t.deepEqual({
-  //   username: 'bender',
-  //   domain: 'ilovebender',
-  //   zone: '.com'
-  // }, regnum('bender@ilovebender.com', '^($username$)@($domain$)($zone$)$', regnumObject), 'matching with email');
+  let regnumObject;
+  let regnum;
+  t.plan(4);
+
+  regnumObject = {number: '[0-9]*'};
+  regnum = new Regnum('($number$)', regnumObject);
+  t.deepEqual({number: '22'}, regnum.match('22'), 'simple test');
+
+  regnumObject = {number: '[0-9]*'};
+  regnum = new Regnum('[a-zA-Z ]*($number$)', regnumObject);
+  t.deepEqual({number: '22'}, regnum.match('Bending Unit 22',), 'testing options');
+
+  regnumObject = {number: '[0-9]*', name: '[a-zA-Z]*'};
+  regnum = new Regnum('($name$).* ($number$)', regnumObject);
+  t.deepEqual({
+    name: 'Bender',
+    number: '22'
+  }, regnum.match('Bender Bending Rodríguez designated in-universe as Bending Unit 22'), '2 object return test');
+
+
+  regnumObject = {
+    username: '[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+',
+    domain: '[a-zA-Z0-9-]+',
+    zone: '(?:\.[a-zA-Z0-9-]+)*'
+  };
+  regnum = new Regnum('^($username$)@($domain$)($zone$)$', regnumObject);
+  t.deepEqual({
+    username: 'bender',
+    domain: 'ilovebender',
+    zone: '.com'
+  }, regnum.match('bender@ilovebender.com'), 'matching with email');
 
 });
